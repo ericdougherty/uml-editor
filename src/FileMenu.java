15353fc3c2e492493f618ce52a43ae282902b147
ROCKETIM\Owner <df@gmail.com>
2016-11-13 16:27:25 -0500

    Exit option and exit fuctionality added to file menu. -T-muthafcking-K

--- src/FileMenu.java
+++ src/FileMenu.java
@@ -1,17 +1,32 @@
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+
 
 public class FileMenu extends MenuBar {
 	
 	public FileMenu(){
 		
-		final Menu menuItem1 = new Menu("File");
-		final Menu menuItem2 = new Menu("Edit");
-		final Menu menuItem3 = new Menu("Preferences");
-		final Menu menuItem4 = new Menu("Help");
-		
-		getMenus().addAll(menuItem1, menuItem2, menuItem3, menuItem4);
+		final Menu menuItem1 = new Menu("File");		
+		final Menu menuItem2 = new Menu("Preferences");
+		final Menu menuItem3 = new Menu("Help");
+		MenuItem exit = new MenuItem("Exit");
+                
+                exit.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent t) {
+                        System.exit(0);
+                    }
+                });
+                
+		getMenus().addAll(menuItem1, menuItem2, menuItem3);
+                menuItem1.getItems().addAll(exit);
 		getStyleClass().add("menu");
+                
+                
 	}
 
 }
